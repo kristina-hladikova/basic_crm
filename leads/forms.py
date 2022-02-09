b1e15c4427f7c7a7ddbedf30c7f935eb98bc8f15
kristi <kristi.lackova@gmail.com>
2022-02-09 10:11:30 +0100

    signup, login, logout

--- leads/forms.py
+++ leads/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth import get_user_model
 from leads.models import Lead
 
+User = get_user_model()
 
 class LeadModelForm(forms.ModelForm):
     class Meta:
@@ -14,4 +16,8 @@ class LeadModelForm(forms.ModelForm):
         )
 
 
-
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {'username': UsernameField}
--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
-from .forms import LeadModelForm
+from .forms import LeadModelForm, CustomUserCreationForm
 
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
